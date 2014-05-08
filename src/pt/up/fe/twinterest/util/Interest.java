5018eb51117b055fe22ba1811068d0b2ecd13bfa
Subrahmanya Somayaji M <somayajisubs@gmail.com>
2014-05-08 12:16:51 +0530

    Update Interest.java

--- src/pt/up/fe/twinterest/util/Interest.java
+++ src/pt/up/fe/twinterest/util/Interest.java
@@ -7,6 +7,77 @@ import android.net.Uri;
 public class Interest {
 	private static final String API_URL = "http://search.twitter.com/search.json";
 	private static final String API_BASE_REFRESH_URL = "?include_entities=true&q=%s";
+	//dummy json since this is depricated twitter api
+	/*
+	
+	{
+    "next_page": "?page=2&max_id=111626724697063424&q=python", 
+    "completed_in": 0.099000000000000005, 
+    "max_id_str": 111626724697063420, 
+    "refresh_url": "?since_id=111626724697063424&q=python", 
+    "results": [
+        {
+            "iso_language_code": "en", 
+            "text": "#Win Christian Louboutin Sueded Python Pump http://t.co/vr5C6St via @AddThis", 
+            "created_at": "Thu, 08 Sep 2011 02:27:39 +0000", 
+            "profile_image_url": "http://xbmc.org/wp-content/uploads/2011/06/zappy-welcome.png", 
+            "from_user_name": "testingnamesamsun", 
+            "from_user": "samsun", 
+            "from_user_id": 32455649, 
+            "geo": null, 
+            "id": 111626724697063420
+        }, 
+        {
+            "iso_language_code": "en", 
+            "text": "#Win Christian Louboutin Sueded Python Pump http://t.co/vr5C6St via @AddThis", 
+            "created_at": "Thu, 08 Sep 2011 02:27:39 +0000", 
+            "profile_image_url": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ17bxuPZB2jqJkz7Lj9N1k1FaXB984VFA5przcwdKg7XEIqOeQ1TcNNNXL", 
+            "from_user_name": "testingnamesachin", 
+            "from_user": "sachin", 
+            "from_user_id": 107, 
+            "geo": null, 
+            "id": 97
+        }, 
+        {
+            "iso_language_code": "en", 
+            "text": "#Win Christian Louboutin Sueded Python Pump http://t.co/vr5C6St via @AddThis", 
+            "created_at": "Thu, 08 Sep 2011 02:27:39 +0000", 
+            "profile_image_url": "http://www.lbwtrust.com.au/wp-content/uploads/2012/07/rahul_dravid.png", 
+            "from_user_name": "testingnamedravid", 
+            "from_user": "dravid", 
+            "from_user_id": 108, 
+            "geo": null, 
+            "id": 98
+        }, 
+        {
+            "iso_language_code": "en", 
+            "text": "#Win Christian Louboutin Sueded Python Pump http://t.co/vr5C6St via @AddThis", 
+            "created_at": "Thu, 08 Sep 2011 02:27:39 +0000", 
+            "profile_image_url": "http://www.bharatdiscovery.org/w/images/6/6c/Sourav-Ganguly.png", 
+            "from_user_name": "testingnameganguly", 
+            "from_user": "ganguly", 
+            "from_user_id": 109, 
+            "geo": null, 
+            "id": 99
+        }, 
+        {
+            "iso_language_code": "en", 
+            "text": "#Win Christian Louboutin Sueded Python Pump http://t.co/vr5C6St via @AddThis", 
+            "created_at": "Thu, 08 Sep 2011 02:27:39 +0000", 
+            "profile_image_url": "http://xbmc.org/wp-content/uploads/2011/06/zappy-welcome.png", 
+            "from_user_name": "testingnamerahane", 
+            "from_user": "rahane", 
+            "from_user_id": 110, 
+            "geo": null, 
+            "id": 100
+        }
+    ], 
+    "query": "python", 
+    "max_id": 111626724697063420, 
+    "page": 1
+}
+	
+	*/
 	
 	private static final String PREF_NAME = "interest";
 	
